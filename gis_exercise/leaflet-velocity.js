53ae4cea741c3508dd8ff41ceef3b3bcca937a42
chchoiw@gmail.com <chchoiw@gmail.com>
2024-01-18 14:48:39 +0800

    up

--- gis_exercise/leaflet-velocity.js
+++ gis_exercise/leaflet-velocity.js
@@ -561,6 +561,7 @@ var Windy = function Windy(params) {
   var PARTICLE_MULTIPLIER=1/700
   // var PARTICLE_LINE_WIDTH=2a
   var VELOCITY_SCALE=0.02;
+  var MAX_PARTICLE_AGE=2;
 
 
   var defaulColorScale = ["rgb(36,104, 180)", "rgb(60,157, 194)", "rgb(128,205,193 )", "rgb(151,218,168 )", "rgb(198,231,181)", "rgb(238,247,217)", "rgb(255,238,159)", "rgb(252,217,125)", "rgb(255,182,100)", "rgb(252,150,75)", "rgb(250,112,52)", "rgb(245,64,32)", "rgb(237,45,28)", "rgb(220,24,32)", "rgb(180,0,35)"];
@@ -592,6 +593,7 @@ var Windy = function Windy(params) {
     if (options.hasOwnProperty("opacity")) OPACITY = +options.opacity;
     if (options.hasOwnProperty("frameRate")) FRAME_RATE = options.frameRate;
     FRAME_TIME = 1000 / FRAME_RATE;
+
   }; // interpolation for vectors like wind (u,v,m)
 
 
