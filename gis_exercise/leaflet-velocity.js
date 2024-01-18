1e1a1a6bf78967ccb996dcda91efadf216bb10ae
chchoiw@gmail.com <chchoiw@gmail.com>
2024-01-18 14:50:10 +0800

    up

--- gis_exercise/leaflet-velocity.js
+++ gis_exercise/leaflet-velocity.js
@@ -561,7 +561,7 @@ var Windy = function Windy(params) {
   var PARTICLE_MULTIPLIER=1/700
   // var PARTICLE_LINE_WIDTH=2a
   var VELOCITY_SCALE=0.02;
-  var MAX_PARTICLE_AGE=2;
+  // var MAX_PARTICLE_AGE=2;
 
 
   var defaulColorScale = ["rgb(36,104, 180)", "rgb(60,157, 194)", "rgb(128,205,193 )", "rgb(151,218,168 )", "rgb(198,231,181)", "rgb(238,247,217)", "rgb(255,238,159)", "rgb(252,217,125)", "rgb(255,182,100)", "rgb(252,150,75)", "rgb(250,112,52)", "rgb(245,64,32)", "rgb(237,45,28)", "rgb(220,24,32)", "rgb(180,0,35)"];
@@ -1262,8 +1262,9 @@ var Windy = function Windy(params) {
       particleCount *= PARTICLE_REDUCTION;
     }
 
-    var fadeFillStyle = "rgba(255,255,255,1)";
-    // "rgba(0, 0, 0, ".concat(OPACITY, ")");
+    var fadeFillStyle = "rgba(0, 0, 0, ".concat(OPACITY, ")");
+    // "rgba(255,255,255,1)";
+    // 
     var particles = [];
 
     for (var i = 0; i < particleCount; i++) {
